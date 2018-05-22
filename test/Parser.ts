e1195904f8734d2d9e19d15aa453bcadfd2161bb
Kevin Ramharak <kevin@ramharak.nl>
2018-05-22 17:12:06 +0200

    added tests for Parser & PreProcessor

---
+++ test/Parser.ts
@@ -0,0 +1,17 @@
+import { deepStrictEqual } from "assert";
+import { assert } from "chai";
+import { describe, it } from "mocha";
+
+import { BooleanToken, CharacterToken, FloatToken, IntegerToken, StringToken, Token, TokenType } from "@/Token";
+
+import { PreProcessorError } from "@/Error";
+import { preProcess, PreProcessorResult } from "@/PreProcessor";
+
+import { TestMissingError } from "@test/Error";
+import { TypeOf } from "@test/util";
+
+describe("Parser", () => {
+    it("This test Suite should be implemented", () => {
+        throw new TestMissingError();
+    });
+});
